299fb46fe04bb41af754f936ff16bf74f208eb9e
User master <toworkallmylife@gmail.com>
2024-04-08 14:31:12 +0300

    add projects

---
+++ Lesson/Игры на WF - Copy (2)/Игры на WF/GameSnake.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Игры_на_WF
+{
+    public partial class GameSnake : Form
+    {
+        private int appleX, appleY;
+        private PictureBox apple;
+        private int _wight = 900;
+        private int _hight = 800;
+        private int _sizeOfOneBlock = 40;
+        private int dirX, dirY;
+
+        public GameSnake()
+        {
+            InitializeComponent();
+            apple = new PictureBox();
+            apple.BackColor = Color.Red;
+            apple.Size = new Size(_sizeOfOneBlock, _sizeOfOneBlock);
+            dirX = 1;
+            dirY = 0;
+            timer1.Tick += new EventHandler(Update);
+            this.Width = _wight;
+            this.Height = _hight;
+            this.KeyDown += new KeyEventHandler(Move);
+            this.KeyDown += new KeyEventHandler(Update);
+            GeneralMap();
+        }
+
+
+        private void Move(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode.ToString())
+            {
+                case "Up":
+                    dirX = 0;
+                    dirY = -1;
+                    break;
+                case "Down":
+                    dirX = 0;
+                    dirY = 1;
+                    break;
+                case "Right":
+                    dirX = 1;
+                    dirY = 0;
+                    break;
+                case "Left":
+                    dirX = -1;
+                    dirY = 0;
+                    break;
+            }
+        }
+
+        private void Update(Object obj, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X + _sizeOfOneBlock * dirX, 
+                pictureBox1.Location.Y + _sizeOfOneBlock * dirY);
+        }
+
+        private void generalApple()
+        {
+            Random r = new Random();
+            //apple = r.Next(0, _wight );
+
+        }
+
+        private void GeneralMap()
+        {
+            int tmp = _wight / _sizeOfOneBlock;
+            int tmp2 = _hight / _sizeOfOneBlock;
+
+            for (int i = 0; i < tmp; i++) 
+            {  
+                PictureBox pictureBox = new PictureBox();
+                pictureBox.BackColor = Color.Black;
+                pictureBox.Location = new Point(0, i * _sizeOfOneBlock);
+                pictureBox.Size = new Size(_wight - 100, 1);
+                this.Controls.Add(pictureBox);
+            }
+
+            for (int i = 0; i <= tmp2; i++)
+            {
+                PictureBox pictureBox = new PictureBox();
+                pictureBox.BackColor = Color.Black;
+                pictureBox.Location = new Point(i * _sizeOfOneBlock, 0);
+                pictureBox.Size = new Size(1, _hight);
+                this.Controls.Add(pictureBox);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GameSnake_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Lesson/Игры на WF - Copy (2)/Игры на WF/GameTicTacToe.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Игры_на_WF
+{
+    public partial class GameTicTacToe : Form
+    {
+        private int step { get; set; } = 1;
+        private int[][] board = { new int[] { 0, 0, 0},
+            new int[] { 0, 0, 0 }, new int[] { 0, 0, 0}};
+
+        public GameTicTacToe()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckWin()
+        {
+            if (board[0][0] ==  board[0][1] && board[0][1] == board[0][2] &&
+                board[0][0] != 0 && board[0][1] != 0 && board[0][2] != 0 ||
+                board[1][0] == board[1][1] && board[1][1] == board[1][2] &&
+                board[1][0] != 0 && board[1][1] != 0 && board[1][2] != 0 ||
+                board[2][0] == board[2][1] && board[2][1] == board[2][2] &&
+                board[2][0] != 0 && board[2][1] != 0 && board[2][2] != 0     ||
+
+                board[0][0] == board[1][0] && board[1][0] == board[2][0] &&
+                board[0][0] != 0 && board[1][0] != 0 && board[2][0] != 0 ||
+                board[0][1] == board[1][1] && board[1][1] == board[2][1] &&
+                board[0][1] != 0 && board[1][1] != 0 && board[2][1] != 0 ||
+                board[0][2] == board[1][2] && board[1][2] == board[2][2] &&
+                board[0][2] != 0 && board[1][2] != 0 && board[2][2] != 0     ||
+
+                board[0][0] == board[1][1] && board[1][1] == board[2][2] &&
+                board[0][0] != 0 && board[1][1] != 0 && board[2][2] != 0 ||
+                board[0][2] == board[1][1] && board[1][1] == board[2][0] &&
+                board[0][2] != 0 && board[1][1] != 0 && board[2][0] != 0
+                )
+            {
+                MessageBox.Show("Win");
+                if(step % 2 ==0)
+                {
+                    label1.Text = (int.Parse(label1.Text) + 1).ToString();
+                }
+                else
+                {
+                    label2.Text = (int.Parse(label2.Text) + 1).ToString();
+                }
+
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+                button5.Text = "";
+                button6.Text = "";
+                button7.Text = "";
+                button8.Text = "";
+                button9.Text = "";
+                board = new int[][]{new int[] { 0, 0, 0 },new int[] { 0, 0, 0 }, new int[] { 0, 0, 0 }};
+                step = 1;
+            }
+        }
+
+        private void GameTicTacToe_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(button1.Text))
+            {
+                if (step % 2 != 0)
+                {
+                    button1.Text = "X";
+                    board[0][0] = -1;
+                    step++;
+                }
+                else
+                {
+                    button1.Text = "O";
+                    board[0][0] = 1;
+                    step++;
+                }
+                CheckWin();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(button2.Text))
+            {
+                if (step % 2 != 0)
+                {
+                    button2.Text = "X";
+                    board[0][1] = -1;
+                    step++;
+                }
+                else
+                {
+                    button2.Text = "O";
+                    board[0][1] = 1;
+                    step++;
+                }
+                CheckWin();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(button3.Text))
+            {
+                if (step % 2 != 0)
+                {
+                    button3.Text = "X";
+                    board[0][2] = -1;
+                    step++;
+                }
+                else
+                {
+                    button3.Text = "O";
+                    board[0][2] = 1;
+                    step++;
+                }
+                CheckWin();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(button6.Text))
+            {
+                if (step % 2 != 0)
+                {
+                    button6.Text = "X";
+                    board[1][0] = -1;
+                    step++;
+                }
+                else
+                {
+                    button6.Text = "O";
+                    board[1][0] = 1;
+                    step++;
+                }
+                CheckWin();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(button5.Text))
+            {
+                if (step % 2 != 0)
+                {
+                    button5.Text = "X";
+                    board[1][1] = -1;
+                    step++;
+                }
+                else
+                {
+                    button5.Text = "O";
+                    board[1][1] = 1;
+                    step++;
+                }
+                CheckWin();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(button4.Text))
+            {
+                if (step % 2 != 0)
+                {
+                    button4.Text = "X";
+                    board[1][2] = -1;
+                    step++;
+                }
+                else
+                {
+                    button4.Text = "O";
+                    board[1][2] = 1;
+                    step++;
+                }
+                CheckWin();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(button9.Text))
+            {
+                if (step % 2 != 0)
+                {
+                    button9.Text = "X";
+                    board[2][0] = -1;
+                    step++;
+                }
+                else
+                {
+                    button9.Text = "O";
+                    board[2][0] = 1;
+                    step++;
+                }
+                CheckWin();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(button8.Text))
+            {
+                if (step % 2 != 0)
+                {
+                    button8.Text = "X";
+                    board[2][1] = -1;
+                    step++;
+                }
+                else
+                {
+                    button8.Text = "O";
+                    board[2][1] = 1;
+                    step++;
+                }
+                CheckWin();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(button7.Text))
+            {
+                if (step % 2 != 0)
+                {
+                    button7.Text = "X";
+                    board[2][2] = -1;
+                    step++;
+                }
+                else
+                {
+                    button7.Text = "O";
+                    board[2][2] = 1;
+                    step++;
+                }
+                CheckWin();
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}

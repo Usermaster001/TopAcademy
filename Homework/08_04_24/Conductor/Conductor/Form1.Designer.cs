15794069989c121a9f6a0875309a4528bf68f644
User master <toworkallmylife@gmail.com>
2024-04-08 15:29:31 +0300

    add Conductor

--- Homework/08_04_24/Conductor/Conductor/Form1.Designer.cs
+++ Homework/08_04_24/Conductor/Conductor/Form1.Designer.cs
@@ -28,13 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(12, 82);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(776, 354);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(665, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(683, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Перейти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Назад";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Homework/08_04_24/Conductor/Conductor/Form1.cs
+++ Homework/08_04_24/Conductor/Conductor/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace Conductor
@@ -15,6 +10,114 @@ namespace Conductor
         public Form1()
         {
             InitializeComponent();
+
+            ViewDrives();
+        }
+
+        // Показ всех дисков
+        public void ViewDrives()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            listBox1.Items.Clear();
+            textBox1.Text = "";
+
+            foreach (DriveInfo drive in drives)
+            {
+                listBox1.Items.Add(drive.Name);
+            }
+        }
+
+        // загрузка папок и файлов, кнопка перейти в директорию
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(textBox1.Text);
+            DirectoryInfo[] directoryInfos = directoryInfo.GetDirectories();
+
+            foreach(DirectoryInfo el in directoryInfos)
+            {
+                listBox1.Items.Add(el);
+            }
+
+            FileInfo[] fileInfos = directoryInfo.GetFiles();
+
+            foreach (FileInfo el in fileInfos)
+            {
+                listBox1.Items.Add(el);
+            }
+        }
+
+
+        // двойной щелчок по элементу listbox
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (Path.GetExtension(Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString())) == "")
+            {
+
+                textBox1.Text = Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString());
+
+                listBox1.Items.Clear();
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(textBox1.Text);
+                DirectoryInfo[] directoryInfos = directoryInfo.GetDirectories();
+
+                foreach (DirectoryInfo el in directoryInfos)
+                {
+                    listBox1.Items.Add(el);
+                }
+
+                FileInfo[] fileInfos = directoryInfo.GetFiles();
+
+                foreach (FileInfo el in fileInfos)
+                {
+                    listBox1.Items.Add(el);
+                }
+            }
+            else
+            {
+                Process.Start(Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString()));
+            }
+        }
+        
+
+        // загрузка папок и файлов, кнопка назад
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 3)
+            {
+                if (textBox1.Text[textBox1.Text.Length - 1] == '\\')
+                {
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+                }
+
+                while (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+                {
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+                }
+
+                listBox1.Items.Clear();
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(textBox1.Text);
+                DirectoryInfo[] directoryInfos = directoryInfo.GetDirectories();
+
+                foreach (DirectoryInfo el in directoryInfos)
+                {
+                    listBox1.Items.Add(el);
+                }
+
+                FileInfo[] fileInfos = directoryInfo.GetFiles();
+
+                foreach (FileInfo el in fileInfos)
+                {
+                    listBox1.Items.Add(el);
+                }
+            }
+            else
+            {
+                ViewDrives();
+            }
         }
     }
 }
